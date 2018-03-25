a7843cc12de6ad4dcd2da2c28e576d482bd4a5f3
lê khánh duy <khanhduyspvdm007@gmail.com>
2018-03-25 15:34:27 +0700

    form đăng ký

--- page-home.php
+++ page-home.php
@@ -78,11 +78,11 @@ get_header();
                                     <form>
                                         <div class="form-group">
                                             <label for="recipient-name" class="col-form-label">Email</label>
-                                            <input type="text" class="form-control" id="recipient-name">
+                                            <input placeholder="Nhập e mail của bạn" type="text" class="form-control" id="recipient-name">
                                         </div>
                                         <div class="form-group">
                                             <label for="message-text" class="col-form-label">Thông Điệp</label>
-                                            <textarea class="form-control" id="message-text"></textarea>
+                                            <textarea placeholder="Nhập tin nhắn của bạn" class="form-control" id="message-text"></textarea>
                                         </div>
                                     </form>
                                 </div>
