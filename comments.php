009904a26e44b3cd69a3ac44eeaf9839f106f192
lê khánh duy <khanhduyspvdm007@gmail.com>
2018-03-28 00:28:57 +0700

    post 1 . chỉ tay cơ bản

--- comments.php
+++ comments.php
@@ -74,9 +74,49 @@ if ( post_password_required() ) {
 
 	endif; // Check for have_comments().
 
-	comment_form();
+
 	?>
 
+<?php if ( comments_open() ) : ?>
+
+<div id="respond" class="comment-respond post">
+<h2 class="title fz18 title2 bg_title1">Để Lại Bình Luận</h2>
+    <div class="cancel-comment-reply">
+    	<small><?php cancel_comment_reply_link(); ?></small>
+    </div>
+
+    <?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
+    <p><a href="<?php echo wp_login_url( get_permalink() ); ?>">Login</a> to comment.</p>
+    <?php else : ?>
+
+<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+
+    <?php if ( is_user_logged_in() ) : ?>
+		<p class="nameuser">Comment with: <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a></p>
+    <?php endif; ?>
+     	<p>
+        	<textarea name="comment" id="comment" cols="50" rows="4" tabindex="4" placeholder="Tin nhắn"></textarea>
+        </p>
+    <?php if(!is_user_logged_in()):?>
+
+
+	      	<input placeholder="Tên của bạn" type="text" name="author" id="author" value="<?php echo esc_attr($comment_author);?>" tabindex="1" <?php if ($req) echo "aria-required='true'"; ?> />
+
+	        <input placeholder="Email của bạn" type="text" name="email" id="email" value="<?php echo esc_attr($comment_author_email); ?>" size="22" tabindex="2" <?php if ($req) echo "aria-required='true'"; ?> />
+
+
+    <?php endif;?>
+                <p class="control"><input class="btn btn-outline-danger" name="submit" type="submit" id="submit" tabindex="5" value="SUBMIT" />
+        <?php comment_id_fields(); ?>
+        </p>
+        <?php do_action('comment_form', $post->ID); ?>
+
+    </form>
+
+        <?php endif; // If registration required and not logged in ?>
+
+    </div>
+<?php endif; // if you delete this the sky will fall on your head ?>
             </div>
     </div>
     <!-- #comments -->
